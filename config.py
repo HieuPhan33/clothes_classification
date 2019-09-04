02ff268930f1cfe108d4f33b5aeac0dc0e1547e8
Steve Phan <hieup007@gmail.com>
2019-09-04 11:53:04 +1000

    first commit

---
+++ config.py
@@ -0,0 +1,19 @@
+import os
+CLASSES = ["1","2","3","4","5","6","7","8","9","10","11","12","13"]
+BASE_PATH='data'
+
+# define the names of the training, testing, and validation
+# directories
+TRAIN = "training"
+VAL = "validation"
+TEST = "evaluation"
+
+# set the batch size when fine-tuning
+BATCH_SIZE = 32
+
+# set the path to the serialized model after training
+MODEL_PATH = os.path.sep.join(["output", "img.model"])
+
+# define the path to the output training history plots
+UNFROZEN_PLOT_PATH = os.path.sep.join(["output", "unfrozen.png"])
+WARMUP_PLOT_PATH = os.path.sep.join(["output", "warmup.png"])
---
+++ model.py
@@ -0,0 +1,47 @@
+# set the matplotlib backend so figures can be saved in the background
+import matplotlib
+matplotlib.use("Agg")
+
+# import the necessary packages
+from keras.preprocessing.image import ImageDataGenerator
+from keras.applications import VGG16
+from keras.layers.core import Dropout
+from keras.layers.core import Flatten
+from keras.layers.core import Dense
+from keras.layers import Input
+from keras.models import Model
+from keras.optimizers import SGD
+from sklearn.metrics import classification_report
+from pyimagesearch import config
+from imutils import paths
+import matplotlib.pyplot as plt
+import numpy as np
+import pickle
+import os
+
+
+def plot_training(H, N, plotPath):
+	# construct a plot that plots and saves the training history
+	plt.style.use("ggplot")
+	plt.figure()
+	plt.plot(np.arange(0, N), H.history["loss"], label="train_loss")
+	plt.plot(np.arange(0, N), H.history["val_loss"], label="val_loss")
+	plt.plot(np.arange(0, N), H.history["acc"], label="train_acc")
+	plt.plot(np.arange(0, N), H.history["val_acc"], label="val_acc")
+	plt.title("Training Loss and Accuracy")
+	plt.xlabel("Epoch #")
+	plt.ylabel("Loss/Accuracy")
+	plt.legend(loc="lower left")
+	plt.savefig(plotPath)
+
+# derive the paths to the training, validation, and testing
+# directories
+trainPath = os.path.sep.join([config.BASE_PATH, config.TRAIN])
+valPath = os.path.sep.join([config.BASE_PATH, config.VAL])
+testPath = os.path.sep.join([config.BASE_PATH, config.TEST])
+
+# determine the total number of image paths in training, validation,
+# and testing directories
+totalTrain = len(list(paths.list_images(trainPath)))
+totalVal = len(list(paths.list_images(valPath)))
+totalTest = len(list(paths.list_images(testPath)))
